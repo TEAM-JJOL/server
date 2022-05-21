00f76e8cacd3ea3464081e1a15ca863b9dfe2f1d
Danna <daeunlee0720@gmail.com>
2022-05-21 15:03:43 +0900

    feat: sample get api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import express, { Request, Response, NextFunction } from 'express';
 
-const app = express();
+const app = express(); // express 객체 받아옴
+
+app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
+
+app.use('/api', require('./api')); // use -> 모든 요청
+// localhost:8000/api -> api 폴더
+// localhost:8000/api/user -> user.ts
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hi! This is my second express server');
+  res.send('Hi!');
 });
 
 app.listen('8000', () => {
   console.log(`
-    ##############################
-       SERVER LISTENING TO 8000
-    ##############################`);
-});
+        #############################################
+            🛡️ Server listening on port: 8000 🛡️
+        #############################################
+    `);
+}); // 8000 번 포트에서 서버를 실행하겠다 ~
